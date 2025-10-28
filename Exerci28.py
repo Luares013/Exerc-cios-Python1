abdd28e143f061ce73771f4feec2525410e65982
LUCAS LUARES DE OLIVEIRA <Lucasfurao5@gmail.com>
2025-10-28 16:52:49 -0400

    Exerci28.py

---
+++ Exerci28.py
@@ -0,0 +1,10 @@
+##----------------------------------------##
+
+
+numero1 = int(input('Digite um número inteiro: '))
+numero2 = int(input('Digite outro número inteiro: '))
+numero3 = float(input('Digite um número real:'))
+
+print(f'Resultado 1: {(2*numero1)*(numero2/2)}')
+print(f'Resultado 2: {numero1 * 3 + numero3}')
+print(f'Resultado 3: {numero3*numero3*numero3}')
